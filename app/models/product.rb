b114a042e752ed0a2f6e40efba91d6fdaf7d63dc
mingliang7 <faithful.7up@gmail.com>
2014-02-15 14:00:43 +0700

    update on sat/feb/15 14:02

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
 	has_many :line_items
 	belongs_to :user
-	before_destroy :ensure_not_referenced_with_any_line_items
+	before_destroy :ensure_not_referenced_with_any_line_items, :check_user
 	validates_presence_of :title, :description, :price
 	validates :price, numericality: {greather_than_or_equal_to: 0.01}
 	validates_uniqueness_of :title
@@ -9,6 +9,8 @@ class Product < ActiveRecord::Base
 		with: %r{\.(jpg|gif|png)\Z}i,
 		message: "have to be a valid image with .gif .jpg .png"
 	}
+
+
 	def ensure_not_referenced_with_any_line_items
 		if line_items.empty?
 			return true
@@ -18,4 +20,17 @@ class Product < ActiveRecord::Base
 		end
 		
 	end
+
+	def check_user
+		current_user = current_user.find_by(product_id: @product)
+		if current_user
+			return true 
+		else
+			errors.add(:base, notice: "you can not delete other post");
+			return false 
+		end
+		
+	end
+
+	
 end
