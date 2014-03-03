119cd84bf4f437821d2576024b30e1e36c3afd5b
mingliang7 <faithful.7up@gmail.com>
2014-03-03 10:42:20 +0700

    reconfig cart in store url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217100528) do
+ActiveRecord::Schema.define(version: 20140301152803) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -78,6 +78,15 @@ ActiveRecord::Schema.define(version: 20140217100528) do
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id"
+
+  create_table "payment_notifications", force: true do |t|
+    t.text     "params"
+    t.string   "status"
+    t.string   "transaction_id"
+    t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
