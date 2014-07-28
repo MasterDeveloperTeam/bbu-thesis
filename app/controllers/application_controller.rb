8011174ba98dd5b580d528b9b63d2cfb97e81cd0
mingliang7 <faithful.7up@gmail.com>
2014-07-28 10:52:17 +0700

    config unallow password_confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :update_sanitized_params, if: :devise_controller?
   protect_from_forgery with: :exception
-private
-	def update_sanitized_params
-	  devise_parameter_sanitizer.for(:sign_up)  << [:email, :password, :password_confirmation]
-	end
+
 end
